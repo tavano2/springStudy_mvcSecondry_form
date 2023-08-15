a0a94a3b69d370e4d92756f3d953dbaef51ea873
tavano2 <asjk158@naver.com>
2023-08-15 21:06:01 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,33 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+    
+    // 판매 여부
+    private Boolean open;
+    // 등록 지역
+    private List<String> regions;
+    // 상품 종류
+    private ItemType itemType;
+    // 배송 방식
+    private String deliveryCode;
+    
+    
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
